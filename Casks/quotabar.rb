e480ee1fe6ab48ac4dfad9115a2a621aa857f31b
chilohwei <chilohwei@gmail.com>
2026-07-06 09:18:24 +0800

    Update cask for v1.1.0 rerelease

--- Casks/quotabar.rb
+++ Casks/quotabar.rb
@@ -1,6 +1,6 @@
 cask "quotabar" do
-  version "1.0.18"
-  sha256 "7abb31cdbc8963cf6640bf3cea89ff9e58ef42cb0c5ba49ed97ffdb3295d799d"
+  version "1.1.0"
+  sha256 "fcd60e4c5081ea40aa1a2587769cffc9670411f7aceddb42dec646f12baae440"
 
   url "https://github.com/chilohwei/QuotaBar/releases/download/v#{version}/QuotaBar-#{version}-universal.dmg"
   name "QuotaBar"
